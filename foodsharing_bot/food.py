d74a73b29942b966a82d486dcef0611d6f0b687f
Evgeny Nasonov <huihurma322@yandex.ru>
2020-06-19 20:43:21 +0300

    architecture

---
+++ foodsharing_bot/food.py
@@ -0,0 +1,4 @@
+
+
+def get_food():
+    print('food')
---
+++ foodsharing_bot/main.py
@@ -0,0 +1,17 @@
+import vk_api
+import bot
+import food
+
+def start_bot():
+    bot.start()
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    start_bot()
